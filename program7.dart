d8605cbc1f7cf0e7b69401267f4293608d23eaa1
kk <138245228+karankeche@users.noreply.github.com>
2024-08-28 10:25:40 +0530

    Update program7.dart

--- program7.dart
+++ program7.dart
@@ -1,1 +1,33 @@
-void main(){
+void main() {
+  int month = 2; 
+  int year = 2024; 
+
+  switch (month) {
+    case 1:
+    case 3:
+    case 5:
+    case 7:
+    case 8:
+    case 10:
+    case 12:
+      print("The month has 31 days.");
+      break;
+    case 4:
+    case 6:
+    case 9:
+    case 11:
+      print("The month has 30 days.");
+      break;
+    case 2:
+      // for leap year
+      if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+        print("February has 29 days.");
+      } else {
+        print("February has 28 days.");
+      }
+      break;
+    default:
+      print("Invalid month number.");
+  }
+}
+
