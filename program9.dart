f6131c7b26b236e4debbc8942a53b3bb1779b5de
kk <138245228+karankeche@users.noreply.github.com>
2024-08-28 10:26:23 +0530

    Update program9.dart

--- program9.dart
+++ program9.dart
@@ -1,2 +1,26 @@
-void main(){
+void main() {
+  String standType = 'middle'; 
+
+  int ticketPrice;
+
+  switch (standType) {
+    case 'upper':
+      ticketPrice = 2000;
+      break;
+    case 'middle':
+      ticketPrice = 3000;
+      break;
+    case 'lower':
+      ticketPrice = 5000;
+      break;
+    case 'other':
+      ticketPrice = 2500;
+      break;
+    default:
+      print("Invalid stand type.");
+      return; 
+  }
+
+  print("The ticket price for the $standType stand is \$${ticketPrice}.");
+}
 
