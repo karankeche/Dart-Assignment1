090ac4526199e76689ab7cd02938f95dfa0762bd
kk <138245228+karankeche@users.noreply.github.com>
2024-08-28 10:26:02 +0530

    Update program8.dart

--- program8.dart
+++ program8.dart
@@ -1,1 +1,14 @@
-void main(){
+void main() {
+  int number = 15; 
+
+  if (number % 3 == 0 && number % 5 == 0) {
+    print("$number is divisible by both 3 and 5.");
+  } else if (number % 3 == 0) {
+    print("$number is divisible by 3.");
+  } else if (number % 5 == 0) {
+    print("$number is divisible by 5.");
+  } else {
+    print("$number is not divisible by 3 or 5.");
+  }
+}
+
